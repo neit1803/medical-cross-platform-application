17c63b7b009e821139204e75f6909eefa1bb5b6a
neit1803 <106059540+neit1803@users.noreply.github.com>
2024-11-12 01:41:16 +0700

    Ceate (Widget: Overview Revenue Chart Card)

---
+++ lib/widgets/boxes/revenue_chart_card.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/config/app_constants.dart';
+import 'package:flutter_application_1/features/statistic/statistic_screen.dart';
+import 'package:flutter_application_1/widgets/chart/multi_lines_chart.dart';
+
+class RevenueChart extends StatefulWidget {
+  double height;
+  BuildContext context;
+  RevenueChart({super.key, required this.context, required this.height});
+
+  @override
+  State<RevenueChart> createState() => _RevenueChartState();
+}
+
+class _RevenueChartState extends State<RevenueChart> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.height,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+        children: [
+          ListTile(
+            title: Row(
+              children: [
+                Text("Revenue analytics", style: Theme.of(context).textTheme.displayLarge,),
+                Spacer(),
+                TextButton(
+                  onPressed: (){
+                     print("Clicked - Navigating to StatisticScreen");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => StatisticScreen(chart: Charts.Pie)),
+                    );
+                  },
+                  child: Text(
+                    "View Details   >",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            trailing: DropdownButton<String>(
+              style: Theme.of(context).textTheme.bodyLarge,
+              value: "Month",
+              items: ["Day", "Month", "Year"].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (_) {},
+            ),
+          ),
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 6,
+                  backgroundColor: Colors.blueAccent,
+                ),
+                SizedBox(width: 5,),
+                Text("Revenue", style: Theme.of(context).textTheme.titleMedium,),
+                SizedBox(width: 15,),
+                CircleAvatar(
+                  radius: 6,
+                  backgroundColor: Colors.purpleAccent,
+                ),
+                SizedBox(width: 5,),
+                Text("Expenses", style: Theme.of(context).textTheme.titleMedium,),
+              ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          Expanded(child: MultiLineChart())
+        ],
+      ),
+    );
+  }
+}
